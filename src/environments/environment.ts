553fade931950adf1eec4910146d15254ca1ead1
Damian Kubiczek <d.kubiczek@reply.com>
2025-08-10 23:13:55 +0200

    Prod configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiBase: 'https://learningplatform2-640690094775.europe-west1.run.app/api/v1'
+    apiBase: 'https://learningplatform2-640690094775.europe-west1.run.app/api/v1',
+    baseHref: '/LearningPlatform2FE/'
   };
   
